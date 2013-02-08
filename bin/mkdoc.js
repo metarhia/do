969c341e4bab10456e87143e3a478d24181c3249
Oleg Slobodskoi <oleg008@gmail.com>
2013-02-08 14:54:09 +0100

    docs

--- bin/mkdoc.js
+++ bin/mkdoc.js
@@ -6,7 +6,10 @@ var dox = require('dox'),
     tpl, readme;
 
 // Don't like the new format Do.method
-apiDoc = apiDoc.replace(/Do\./g, 'Do#').trim();
+apiDoc = apiDoc
+    .replace(/Do\./g, 'Do#')
+    .replace('##', '###')
+    .trim();
 tpl = fs.readFileSync(__dirname + '/readme.tpl', 'utf8');
 readme = tpl.replace('{api}', apiDoc);
 fs.writeFileSync(__dirname + '/../readme.md', readme);
