e8a91ea0cd3162cdd1b247c51a0d24cd47b6d51a
tshemsedinov <timur.shemsedinov@gmail.com>
2017-12-10 20:20:07 +0000

    Fix qunit and eslint issues
    PR-URL: https://github.com/metarhia/do/pull/2

--- bin/mkdoc.js
+++ bin/mkdoc.js
@@ -1,17 +1,21 @@
-var dox = require('dox'),
-    fs = require('fs'),
-    code = fs.readFileSync(__dirname + '/../index.js').toString('utf8'),
-    ast = dox.parseComments(code, {raw: true}),
-    apiDoc = dox.api(ast),
-    tpl, readme;
+'use strict';
+
+const dox = require('dox');
+const fs = require('fs');
+
+const code = fs.readFileSync(__dirname + '/../index.js').toString('utf8');
+const ast = dox.parseComments(code, { raw: true });
+
+let apiDoc = dox.api(ast);
 
 // Don't like the new format Do.method
 apiDoc = apiDoc
     .replace(/Do\./g, 'Do#')
     .replace('##', '###')
     .trim();
-tpl = fs.readFileSync(__dirname + '/readme.tpl', 'utf8');
-readme = tpl.replace('{api}', apiDoc);
+
+const tpl = fs.readFileSync(__dirname + '/readme.tpl', 'utf8');
+const readme = tpl.replace('{api}', apiDoc);
 fs.writeFileSync(__dirname + '/../readme.md', readme);
 
 console.log('Written readme.md');
