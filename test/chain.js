ee18a271593d0a9894c61cd80ca7227a29cc47b4
mapogolions <ivanovpavelalex45@gmail.com>
2021-03-27 01:01:39 +0500

    update tests

--- test/chain.js
+++ test/chain.js
@@ -10,6 +10,31 @@ const wrapAsync = (
   setTimeout(callback, Math.floor(Math.random() * 1000));
 };
 
+metatests.test('first occured error should break chain & call done', (test) => {
+  const readConfig = (name, callback) => {
+    wrapAsync(() => {
+      callback(new Error('something went wrong'));
+    });
+  };
+
+  const selectFromDb = (query, { name }, callback) => {
+    test.strictSame(name, 'myConfig');
+    wrapAsync(() => {
+      callback(null, query);
+    });
+  };
+
+  const c1 = chain
+    .do(readConfig, 'myConfig')
+    .do(selectFromDb, 'select * from cities');
+
+  c1((err, data) => {
+    test.strictSame(err.message, 'something went wrong');
+    test.strictSame(data, undefined);
+    test.end();
+  });
+});
+
 metatests.test('last call terminate chain', (test) => {
   const readConfig = (name, callback) => {
     test.strictSame(name, 'myConfig');
@@ -28,40 +53,40 @@ metatests.test('simple chain/do', (test) => {
   const readConfig = (name, callback) => {
     test.strictSame(name, 'myConfig');
     wrapAsync(() => {
-      callback(null, { name });
+      callback(null); // should be able to pass nothing
     });
   };
 
   const selectFromDb = (query, callback) => {
     test.strictSame(query, 'select * from cities');
     wrapAsync(() => {
-      callback(null, [{ name: 'Kiev' }, { name: 'Roma' }]);
+      callback(null, { protocol: 'http' });
     });
   };
 
-  const getHttpPage = (url, callback) => {
-    test.strictSame(url, 'http://kpi.ua');
+  const getHttpPage = (host, { protocol }, callback) => {
+    test.strictSame(`${protocol}://${host}`, 'http://kpi.ua');
     wrapAsync(() => {
       callback(null, '<html>Some archaic web here</html>');
     });
   };
 
-  const readFile = (path, callback) => {
+  const writeToFile = (path, content, callback) => {
     test.strictSame(path, 'README.md');
     wrapAsync(() => {
-      callback(null, 'file content');
+      callback(null, { bytesWritten: content.length });
     });
   };
 
   const c1 = chain
     .do(readConfig, 'myConfig')
     .do(selectFromDb, 'select * from cities')
-    .do(getHttpPage, 'http://kpi.ua')
-    .do(readFile, 'README.md');
+    .do(getHttpPage, 'kpi.ua')
+    .do(writeToFile, 'README.md');
 
-  c1((err, result) => {
+  c1((err, { bytesWritten }) => {
     test.strictSame(err, null);
-    test.strictSame(result, 'file content');
+    test.strictSame(bytesWritten, 34);
     test.end();
   });
 });
